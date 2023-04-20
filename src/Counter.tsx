4ddc7fabda97e8d2741dcdd0f22d71c7e7c8dac4
kolyamba2105 <haravetskyy@gmail.com>
2023-04-21 00:44:09 +0300

    Update counter

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -6,9 +6,9 @@ export function Counter() {
   return (
     <button
       className="rounded border border-indigo-400 bg-gradient-to-r from-pink-400 to-sky-600 bg-clip-text p-4 text-2xl text-transparent"
-      disabled={count === 0}
+      disabled={count < 0}
       onClick={() => setCount((count) => count - 1)}>
-      {count === 0 ? "Good job!" : `Click me ${count} more times`}
+      {count < 0 ? "Good job!" : `Click me ${count} more times`}
     </button>
   );
 }
